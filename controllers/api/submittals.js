4aa3ea73bb9b47642f38ebfe5c1167ecec2eef56
Napo <Michaeljnapolitano@gmail.com>
2021-03-05 12:49:58 -0500

    got company ID and my project

--- controllers/api/submittals.js
+++ controllers/api/submittals.js
@@ -2,14 +2,18 @@ const router = require("express").Router();
 const fetch = require("node-fetch");
 require("dotenv").config();
 
+const projectId = "3fd8e539-0be9-4706-a8a2-6b057fbea2dd";
+
+const submittalsPath = `https://sandbox.procore.com/rest/v1.0/projects/${projectId}/submittals`;
+const companyPath = "https://sandbox.procore.com/rest/v1.0/companies"
+const projectsPath = "https://sandbox.procore.com/rest/v1.0/projects?company_id=27788"
 const body = {
   grant_type: "client_credentials",
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
 };
 
-router.post("/auth", (req, res) => {
-  // try {
+router.post("/", (req, res) => {
   fetch("https://sandbox.procore.com/oauth/token", {
     method: "post",
     body: JSON.stringify(body),
@@ -18,13 +22,15 @@ router.post("/auth", (req, res) => {
     .then((res) => res.json())
     .then((tokenData) => {
       console.log(tokenData);
-      // res.json(tokenData);
-      return fetch("https://sandbox.procore.com//rest/v1.0/companies", {
-        headers: {
-          Authorization: tokenData.token_type + " " + tokenData.access_token,
-          "Content-Type": "application/json",
-        },
-      });
+      return fetch(
+        projectsPath,
+        {
+          headers: {
+            Authorization: tokenData.token_type + " " + tokenData.access_token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
     })
     .then((res) => res.json())
     .then((companyData) => {
